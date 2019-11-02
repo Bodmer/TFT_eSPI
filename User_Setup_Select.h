425a4744a7c44682300949de34eea024740b37e8
Bodmer <bodmer@anola.net>
2019-11-02 13:24:58 +0000

    Revert pin references #456

--- User_Setup_Select.h
+++ User_Setup_Select.h
@@ -128,15 +128,15 @@
 
 // These are the pins for ESP8266 boards
 //      Name   GPIO    NodeMCU      Function
-#define PIN_D0  D0  // GPIO16       WAKE
-#define PIN_D1  D1  // GPIO5        User purpose
-#define PIN_D2  D2  // GPIO4        User purpose
-#define PIN_D3  D3  // GPIO0        Low on boot means enter FLASH mode
-#define PIN_D4  D4  // GPIO2        TXD1 (must be high on boot to go to UART0 FLASH mode)
-#define PIN_D5  D5  // GPIO14       HSCLK
-#define PIN_D6  D6  // GPIO12       HMISO
-#define PIN_D7  D7  // GPIO13       HMOSI  RXD2
-#define PIN_D8  D8  // GPIO15       HCS    TXD0 (must be low on boot to enter UART0 FLASH mode)
+#define PIN_D0  16  // GPIO16       WAKE
+#define PIN_D1   5  // GPIO5        User purpose
+#define PIN_D2   4  // GPIO4        User purpose
+#define PIN_D3   0  // GPIO0        Low on boot means enter FLASH mode
+#define PIN_D4   2  // GPIO2        TXD1 (must be high on boot to go to UART0 FLASH mode)
+#define PIN_D5  14  // GPIO14       HSCLK
+#define PIN_D6  12  // GPIO12       HMISO
+#define PIN_D7  13  // GPIO13       HMOSI  RXD2
+#define PIN_D8  15  // GPIO15       HCS    TXD0 (must be low on boot to enter UART0 FLASH mode)
 #define PIN_D9   3  //              RXD0
 #define PIN_D10  1  //              TXD0
 
