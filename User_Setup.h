ea6c47d928ef8d0c4ecedc4f9a4ed54f73f55efd
Just Call Me Koko <25190487+justcallmekoko@users.noreply.github.com>
2020-02-03 19:26:23 -0500

    Create TFT_Button_Label_Datum.ino

--- User_Setup.h
+++ User_Setup.h
@@ -164,12 +164,12 @@
 // For ESP32 Dev board (only tested with ILI9341 display)
 // The hardware SPI can be mapped to any pins
 
-//#define TFT_MISO 19
-//#define TFT_MOSI 23
-//#define TFT_SCLK 18
-//#define TFT_CS   15  // Chip select control pin
-//#define TFT_DC    2  // Data Command control pin
-//#define TFT_RST   4  // Reset pin (could connect to RST pin)
+//#define TFT_MISO 19 // Matching T_DO
+//#define TFT_MOSI 23 // Matching T_DIN
+//#define TFT_SCLK 18 // Matching T_CLK
+//#define TFT_CS   17  // Chip select control pin
+//#define TFT_DC   16  // Data Command control pin
+//#define TFT_RST   5  // Reset pin (could connect to RST pin)
 //#define TFT_RST  -1  // Set TFT_RST to -1 if display RESET is connected to ESP32 board RST
 
 //#define TFT_BL   32  // LED back-light (only for ST7789 with backlight control pin)
