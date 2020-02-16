55e97ffe3391093257b4feea1afa59cf18cff337
Bodmer <Bodmer@users.noreply.github.com>
2020-02-16 18:50:39 +0000

    Fix #539

--- Processors/TFT_eSPI_ESP32.c
+++ Processors/TFT_eSPI_ESP32.c
@@ -443,7 +443,7 @@ void TFT_eSPI::pushPixels(const void* data_in, uint32_t len){
 void TFT_eSPI::pushBlock(uint16_t color, uint32_t len){
   if ( (color >> 8) == (color & 0x00FF) )
   { if (!len) return;
-    tft_Write_16(color); WR_L; WR_H;
+    tft_Write_16(color);
     while (--len) {WR_L; WR_H; WR_L; WR_H;}
   }
   else while (len--) {tft_Write_16(color);}
