e27ee952f1f4396415c98ee8d21e162aecc49d5c
Bodmer <Bodmer@users.noreply.github.com>
2020-03-28 12:39:22 +0000

    Update Setup45_TTGO_T_Watch.h
    80MHz is unreliable for some code sequences.

--- User_Setups/Setup45_TTGO_T_Watch.h
+++ User_Setups/Setup45_TTGO_T_Watch.h
@@ -23,8 +23,8 @@
 // #define SPI_FREQUENCY  10000000
 // #define SPI_FREQUENCY  20000000
 // #define SPI_FREQUENCY 27000000 // Actually sets it to 26.67MHz = 80/3
-// #define SPI_FREQUENCY  40000000 // Maximum to use SPIFFS
-#define SPI_FREQUENCY  80000000
+#define SPI_FREQUENCY  40000000
+//#define SPI_FREQUENCY  80000000
 
 
 
