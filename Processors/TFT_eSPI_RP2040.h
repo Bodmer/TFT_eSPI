534372ef9950e61a019d7b2edec4e8d5a44c8c09
Bodmer <bodmer@anola.net>
2021-04-12 19:39:23 +0100

    Fix  errors with "Arduino Mbed OS RP2040 Boards"
    Arduino RP2040 support introduces a new issue.
    
    This is a temporary fix.

--- Processors/TFT_eSPI_RP2040.h
+++ Processors/TFT_eSPI_RP2040.h
@@ -192,5 +192,46 @@
   #define tft_Read_8() spi.transfer(0)
 #endif
 
+////////////////////////////////////////////////////////////////////////////////////////
+// Temporary to keep the "Arduino Mbed OS RP2040 Boards" support package happy
+////////////////////////////////////////////////////////////////////////////////////////
+  #if defined(ARDUINO_ARCH_RP2040)
+    #ifndef D0
+      #define D0   0
+      #define D1   1
+      #define D2   2
+      #define D3   3
+      #define D4   4
+      #define D5   5
+      #define D6   6
+      #define D7   7
+      #define D8   8
+      #define D9   9
+      #define D10 10
+      #define D11 11
+      #define D12 12
+      #define D13 13
+      #define D14 14
+      #define D15 15
+      #define D16 16
+      #define D17 17
+      #define D18 18
+      #define D19 19
+      #define D20 20
+      #define D21 21
+      #define D22 22
+      #define D23 23
+      #define D24 24
+      #define D25 25
+      #define D26 26
+      #define D27 27
+      #define D28 28
+      #define D29 29
+      #define D30 30
+    #endif
+
+    #define ltoa itoa
+
+  #endif
 
 #endif // Header end
