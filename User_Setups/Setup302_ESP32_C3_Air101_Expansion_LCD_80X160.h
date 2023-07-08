65d5576c960b6a5e6d1e60ed36394a9e8f840d69
Xk-w <97942064+Xk-w@users.noreply.github.com>
2023-07-08 16:01:02 +0800

    Update Setup302_ESP32_C3_Air101_Expansion_LCD_80X160.h

--- User_Setups/Setup302_ESP32_C3_Air101_Expansion_LCD_80X160.h
+++ User_Setups/Setup302_ESP32_C3_Air101_Expansion_LCD_80X160.h
@@ -2,7 +2,10 @@
 #define USER_SETUP_ID 302
 
 #define ST7735_DRIVER
-#define ST7735_GREENTAB160x80 // For 160 x 80 display (BGR, inverted, 26 offset)
+
+// Different offsets are used depending on the generation of screens
+#define ST7735_GREENTAB160x80 // ESP32_C3_Air101_Expansion_LCD_80X160 New Configuration
+// #define ST7735_REDTAB160x80   // ESP32_C3_Air101_Expansion_LCD_80X160 Old Configuration
 
 #define TFT_WIDTH 80
 #define TFT_HEIGHT 160
@@ -14,10 +17,10 @@
 #define TFT_MISO -1
 #define TFT_MOSI 3
 #define TFT_SCLK 2
-#define TFT_CS    7  // Chip select control pin
-#define TFT_DC    6  // Data Command control pin
-#define TFT_RST   10  // Reset pin (could connect to RST pin)
-#define TFT_BL   11  // LED back-light
+#define TFT_CS 7   // Chip select control pin
+#define TFT_DC 6   // Data Command control pin
+#define TFT_RST 10 // Reset pin (could connect to RST pin)
+#define TFT_BL 11  // LED back-light
 
 #define LOAD_GLCD  // Font 1. Original Adafruit 8 pixel font needs ~1820 bytes in FLASH
 #define LOAD_FONT2 // Font 2. Small 16 pixel high font, needs ~3534 bytes in FLASH, 96 characters
@@ -32,7 +35,7 @@
 
 // #define SPI_FREQUENCY  20000000
 // #define SPI_FREQUENCY 27000000
-#define SPI_FREQUENCY  40000000
+#define SPI_FREQUENCY 40000000
 
 #define SPI_TOUCH_FREQUENCY 2500000
 
