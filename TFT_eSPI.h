1d25eda1ccd6eb034471950d998f727ade26bbab
Tuanee2 <buiquoctuan18102002@gmail.com>
2024-06-18 13:58:10 +0700

    Add selection grid feature for TFT_eSPI

--- TFT_eSPI.h
+++ TFT_eSPI.h
@@ -517,6 +517,8 @@ class TFT_eSPI : public Print { friend class TFT_eSprite; // Sprite class has ac
            drawRect(int32_t x, int32_t y, int32_t w, int32_t h, uint32_t color),
            drawRoundRect(int32_t x, int32_t y, int32_t w, int32_t h, int32_t radius, uint32_t color),
            fillRoundRect(int32_t x, int32_t y, int32_t w, int32_t h, int32_t radius, uint32_t color);
+           drawRoundRectWithText(int32_t x, int32_t y, int32_t w, int32_t h, int32_t r, uint32_t rectColor, const char* label, uint32_t textColor, uint8_t textDatum);
+           drawSelectionGrid(int32_t x, int32_t y, int32_t buttonWidth, int32_t buttonHeight, int32_t buttonSpacing, uint32_t colorNormal, uint32_t colorSelection, int32_t numOfSelect, const char* labels[], int32_t numLabels);
 
   void     fillRectVGradient(int16_t x, int16_t y, int16_t w, int16_t h, uint32_t color1, uint32_t color2);
   void     fillRectHGradient(int16_t x, int16_t y, int16_t w, int16_t h, uint32_t color1, uint32_t color2);
