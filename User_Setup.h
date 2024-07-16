0f12e54d6046c186048be6eebeaf0c4e1b77c628
michael <michael@dufflecoat.io>
2024-07-16 21:28:06 +1000

    - tweak LED colours - dispense faster - holes for cartridge output better positioned - other things ive forgotten

--- User_Setup.h
+++ User_Setup.h
@@ -132,7 +132,7 @@
 // driven with a PWM signal or turned OFF/ON then this must be handled by the user
 // sketch. e.g. with digitalWrite(TFT_BL, LOW);
 
-#define TFT_BL   26            // LED back-light control pin
+#define TFT_BL   25            // LED back-light control pin
 #define TFT_BACKLIGHT_ON HIGH  // Level to turn ON back-light (HIGH or LOW)
 
 
